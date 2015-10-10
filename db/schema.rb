59a0f487858adac954c949595e617ca7dea15246
Channing <cchanningallen@gmail.com>
2015-10-10 12:21:39 -0600

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20141117030658) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "events", force: true do |t|
+  create_table "events", force: :cascade do |t|
     t.text     "name"
     t.date     "date"
     t.text     "time"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20141117030658) do
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
